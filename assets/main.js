a2058c2b4731bf8dd401b56740d05dd7327e7663
okumushelton1@gmail.com <okumushelton1@gmail.com>
2022-06-21 10:58:17 +0000

    Code running

---
+++ assets/main.js
@@ -0,0 +1,6 @@
+function formdata() 
+{
+var firstname1= document.getElementById("firstname").value;
+document.writeln("<h1>Hello World!</h1><br>");
+document.writeln("Hello " + firstname1 + ", welcome to the bootcamp<br>");
+}
